bcc713c843914bc090598b3c5ff88cc3c6cbb07a
Daniel Frishberg <frishber@ncbi.nlm.nih.gov>
2013-08-30 15:14:41 -0400

    WT-324: Added esummary to eutils.py

--- eutils.py
+++ eutils.py
@@ -7,14 +7,24 @@ __version__ = '0.1.1'
 _eutils_base = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
 
 
-def esearch(db, id):
+def esearch(db, term):
     response = requests.get(_eutils_base + 'esearch.fcgi',
                             params={
                                 'db': db,
-                                'term': id,
+                                'term': term,
                             })
 
     esearch_doc = etree.fromstring(response.content)
     ids = [id_el.text for id_el in etree.XPath('/eSearchResult/IdList/Id')(esearch_doc)]
 
     return ids
+
+def esummary(db, id):
+    response = requests.get(_eutils_base + 'esummary.fcgi',
+                            params={
+                                'db': db,
+                                 'id': id,
+                                 "version": "2.0"
+                            })
+
+    return response.content
